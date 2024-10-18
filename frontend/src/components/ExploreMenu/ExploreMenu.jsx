77636b766ec6da9ab346ac56d10107d332876b0a
divyanshpanwar03 <divyansh.panwar2003@gmail.com>
2024-10-18 20:49:15 +0530

    created explore menu

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our Menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingrediants and culinary expertise. Our Mission is to become your first choice in ordering your favorite food online whenever you feel hungry, so stop starving & start ordering your favorite food</p>
+        <div className="explore-menu-list">
+          {menu_list.map((item,index)=>{
+            return (
+              <div onClick={()=>setCategory(prev=> prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                <img className={category===item.menu_name?"active":""}src={item.menu_image} alt="" />
+                <p>{item.menu_name}</p>
+              </div>  
+            )
+          })}
+        </div>
+        <hr />  
+    </div>
+  )
+}
+
+export default ExploreMenu
