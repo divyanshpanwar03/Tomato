a1decb63436dcb4e67e64cdea1e8d734979a4a2f
divyanshpanwar03 <divyansh.panwar2003@gmail.com>
2024-11-12 00:39:26 +0530

    fixed some issues

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "tomato-fy7i.vercel.app"
+    const url = "http://localhost:4000"
     const [token,setToken]= useState("");
 
     const [food_list,setFoodList]=useState([])
