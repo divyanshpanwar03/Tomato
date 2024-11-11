94dd1b9395e0e63a1808bc367edad8fa66af9150
divyanshpanwar03 <divyansh.panwar2003@gmail.com>
2024-11-12 00:27:56 +0530

    yes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cartRouter from './routes/cartRoute.js';
 
 // App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
